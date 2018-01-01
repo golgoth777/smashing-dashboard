f4857857c8c29c346a3d3d60cbd6bd2b9d3a0c93
gautier <golgoth777@gmail.com>
2018-01-01 18:52:48 +0000

    passage  current: dans le send _event

--- jobs/netdata_rpi1.rb
+++ jobs/netdata_rpi1.rb
@@ -9,7 +9,7 @@ words = ["system.cpu", "ram.used", "ram.free", "system.iowait", "system.load"]
 #url = "http://remote.nicolleau.eu:19999/v1/gifs/search?q="+ word +"&api_key=dc6zaTOxFJmzC&limit=100"
 for word in words
 url = "http://remote.nicolleau.eu:19999/api/v1/data?chart="+ word +"&after=-60&group=average"
-send_event('netdata_rpi1', {title: word, data: getSvg(url)})
+send_event('netdata_rpi1', {current: getSvg(url)})
 end
  
 end
