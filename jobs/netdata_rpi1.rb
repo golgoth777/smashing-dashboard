0c7352419a23b494e3abce633c5608408a53fc7f
gautier <golgoth777@gmail.com>
2018-01-01 19:28:23 +0000

    passage en chart plutot que data

--- jobs/netdata_rpi1.rb
+++ jobs/netdata_rpi1.rb
@@ -8,7 +8,7 @@ words = ["system.cpu", "ram.used", "ram.free", "system.iowait", "system.load"]
  
 #url = "http://remote.nicolleau.eu:19999/v1/gifs/search?q="+ word +"&api_key=dc6zaTOxFJmzC&limit=100"
 for word in words
-url = "http://remote.nicolleau.eu:19999/api/v1/data?chart="+ word +"&after=-60&group=average"
+url = "http://remote.nicolleau.eu:19999/api/v1/chart?chart="+ word +"&after=-60&group=average"
 send_event('netdata_rpi1', {current: getSvg(url)})
 end
  
