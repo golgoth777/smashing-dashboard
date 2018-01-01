f2ada5381ba1fa403ace0e310c7979b6f691adb1
gautier <golgoth777@gmail.com>
2018-01-01 20:31:27 +0000

    test pour grer les instancesde netdata

--- jobs/netdata_rpi1.rb
+++ jobs/netdata_rpi1.rb
@@ -3,15 +3,18 @@ require 'httparty'
 require 'json'
  
 SCHEDULER.every '1m', :first_in => 0 do |job|
- 
+
+servers = ["remote.nicolleau.eu:19999","remote.nicolleau.eu:29999","remote.nicolleau.eu:39999","remote.nicolleau.eu:49999","remote.nicolleau.eu:59999"]
 words = ["system.cpu", "ram.used", "ram.free", "system.iowait", "system.load"]
  
 #url = "http://remote.nicolleau.eu:19999/v1/gifs/search?q="+ word +"&api_key=dc6zaTOxFJmzC&limit=100"
-for word in words
-url = "http://remote.nicolleau.eu:19999/api/v1/chart?chart="+ word +"&after=-10&group=average"
-send_event('netdata_rpi1', {current: getSvg(url)})
-end
- 
+for i in servers.length()
+	for server in servers
+		for word in words
+			url = "http://"+ server +"/api/v1/data?chart="+ word +"&after=-10&group=average"
+			send_event('netdata_rpi1', {current: getSvg(url)})
+		end
+	end
 end
  
 def getSvg(apiUrl)
