1d8d7897c4460b944f24b3bbb72d3b6774fe9aae
gautier <golgoth777@gmail.com>
2018-01-01 21:33:24 +0000

    tableau des serveur en comm

--- jobs/netdata_rpi1.rb
+++ jobs/netdata_rpi1.rb
@@ -4,7 +4,8 @@ require 'json'
  
 SCHEDULER.every '1m', :first_in => 0 do |job|
 
-servers = ["remote.nicolleau.eu:19999","remote.nicolleau.eu:29999","remote.nicolleau.eu:39999","remote.nicolleau.eu:49999","remote.nicolleau.eu:59999"]
+servers = ["remote.nicolleau.eu:19999"]
+#,"remote.nicolleau.eu:29999","remote.nicolleau.eu:39999","remote.nicolleau.eu:49999","remote.nicolleau.eu:59999"]
 words = ["system.cpu", "system.used", "system.entropy", "system.iowait", "system.load"]
  
 #url = "http://remote.nicolleau.eu:19999/v1/gifs/search?q="+ word +"&api_key=dc6zaTOxFJmzC&limit=100"
@@ -12,7 +13,7 @@ words = ["system.cpu", "system.used", "system.entropy", "system.iowait", "system
 	for server in servers
 		for word in words
 			url = "http://"+ server +"/api/v1/data?chart="+ word +"&after=-10&group=average"
-			send_event('netdata_rpi1', {current: getSvg(url)})
+			send_event('netdata_rpi1', {title: word, current: getSvg(url)})
 		end
 	end
   #end
