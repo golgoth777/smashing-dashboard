dfad710a0bf4b42c631ced34ac0f8f670e2a4d06
gautier <golgoth777@gmail.com>
2018-01-01 18:32:15 +0000

    ajout de la boucle pour recuprer le svg de l\'api netdata

--- jobs/netdata_rpi1.rb
+++ jobs/netdata_rpi1.rb
@@ -1,4 +1,22 @@
-# :first_in sets how long it takes before the job is first run. In this case, it is run immediately
+require 'httparty'
+ 
+require 'json'
+ 
 SCHEDULER.every '1m', :first_in => 0 do |job|
-  send_event('widget_id', { })
+ 
+words = ["system.cpu", "ram.used", "ram.free", "system.iowait", "system.load"]
+ 
+#url = "http://remote.nicolleau.eu:19999/v1/gifs/search?q="+ word +"&api_key=dc6zaTOxFJmzC&limit=100"
+for word in words
+url = "http://remote.nicolleau.eu:19999/api/v1/data?chart="+ word +"&after=-60&group=average"
+send_event('netdata_rpi1', {title: word, netdata-rpi1: getSvg(url)})
+loop
+ 
 end
+ 
+def getSvg(apiUrl)
+res = HTTParty.get(apiUrl)
+list = res["data"]
+return getItem(list)["images"]["original"]["url"]
+end
+ 
